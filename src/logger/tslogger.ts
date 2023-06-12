3b4508df834cec55dcea7347077ecc4f27ec0f2e
Michal Roman <michal.roman@epptec.eu>
2023-06-12 17:48:44 +0200

    extend: logger

--- src/logger/tslogger.ts
+++ src/logger/tslogger.ts
@@ -1,12 +1,69 @@
+import {checkRouteMethods} from '../utils/checkRouteMethods'
+import colors from 'colors'
+
 class DRLogger {
 
-    constructor() {}
+  constructor() {
+  }
 
-    public info = console.log;
-    public success = console.log;
-    public warn = console.warn;
-    public error = console.error;
-    public fatal = console.error;
+  public info = console.log
+  public success = console.log
+  public warn = console.warn
+  public error = console.error
+  public fatal = console.error
+  private now = new Date()
+
+  infoLog = (req) => {
+    const {model, method} = checkRouteMethods(req)
+    console.log(
+      colors.cyan('[INFO]'),
+      'Model:' + colors.cyan(model),
+      'Method:' + colors.cyan(method),
+      'Body:' + JSON.stringify(req.body))
+  }
+
+  successLog = (req) => {
+    const {model, method} = checkRouteMethods(req)
+    console.log(
+      colors.green('[SUCCESS]'),
+      this.now,
+    'Model:' + colors.green(model),
+      'Method:' + colors.green(method),
+      'Body:' + JSON.stringify(req.body))
+  }
+
+  warnLog = (req) => {
+    const {model, method} = checkRouteMethods(req)
+    console.warn(
+      colors.yellow('[WARN]'),
+      this.now,
+      'Model:' + colors.yellow(model),
+      'Method:' + colors.yellow(method),
+      'Body:' + JSON.stringify(req.body))
+  }
+
+  errorLog = (req, message?) => {
+    const {model, method} = checkRouteMethods(req)
+    console.error(
+      colors.red('[ERROR]'),
+      this.now,
+      'Model:' + colors.red(model),
+      'Method:' + colors.red(method),
+      'Body:' + JSON.stringify(req.body),
+      'Message:' + colors.red(message || ''))
+  }
+
+  fatalLog = (req) => {
+    const {model, method} = checkRouteMethods(req)
+    console.error(
+      colors.red('[FATAL]'),
+      this.now,
+      'Model:' + colors.red(model),
+      'Method:' + colors.red(method),
+      'Body:' + JSON.stringify(req.body))
+  }
 }
 
-export const logger = new DRLogger();
+export const logger = new DRLogger()
+
+
