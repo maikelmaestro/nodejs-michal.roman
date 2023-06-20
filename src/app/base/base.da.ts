647dd8777af2ba894b44f58b56a8d3616d9c51f4
Michal Roman <michal.roman@epptec.eu>
2023-06-20 13:36:23 +0200

    fix: implemented redis in DA

--- src/app/base/base.da.ts
+++ src/app/base/base.da.ts
@@ -3,24 +3,25 @@ import {DBConnections} from '../../services/databases/DBConnections'
 import {IBaseItem} from './base.model'
 import {HttpException} from '../../exceptions/HttpException'
 import {redisCache} from '../cache/redis'
+import {generateRedisKey} from '../utils/generateRedisKey'
 
 export class BaseDa<T extends IBaseItem, DTO> {
   protected database: Db
-  private cacheKey: string = this.collectionName
+  private cachePrefix: string = this.collectionName
 
   constructor(protected collectionName: string) {
     this.getDB()
   }
 
   async find(query: {filter: any, sort: any}): Promise<T[]> {
-    // TODO: Fetch from cache fix if filter used
-    // const cacheData = await redisCache.getJSON(this.cacheKey)
+    const cacheKey: string = generateRedisKey(this.cachePrefix, query.filter)
+    const cacheData = await redisCache.getJSON(cacheKey)
 
-    // if (cacheData) return cacheData
+    if (cacheData) return cacheData
 
     try {
       const data = await this.database.collection(this.collectionName).find(query.filter).sort(query.sort).toArray() as T[]
-      // await redisCache.setJSON(this.cacheKey, data)
+      await redisCache.setJSON(cacheKey, data)
       return data
     } catch (error) {
       throw new HttpException(error.status, `Unable to find ${this.collectionName} items`)
@@ -28,7 +29,9 @@ export class BaseDa<T extends IBaseItem, DTO> {
   }
 
   async findOne(id: ObjectId): Promise<T> {
-    let found: T = await redisCache.getJSON(`${this.cacheKey}:${id}`)
+    const cacheKey = generateRedisKey(this.cachePrefix)
+
+    let found: T = await redisCache.getJSON(`${cacheKey}:${id}`)
 
     if (found) return found
 
@@ -42,14 +45,15 @@ export class BaseDa<T extends IBaseItem, DTO> {
       throw new HttpException(404, `Unable to find ${this.collectionName} with id ${id}`)
     }
 
-    await redisCache.setJSON(`${this.cacheKey}:${id}`, found)
+    await redisCache.setJSON(`${cacheKey}:${id}`, found)
     return found
   }
 
   async createOne(payload: DTO): Promise<{id: ObjectId}> {
+
     try {
       const created = await this.database.collection(this.collectionName).insertOne(payload)
-      // await redisCache.del(`${this.cacheKey}`)
+      await redisCache.reset()
       return {id: created.insertedId}
     } catch (error) {
       throw new HttpException(error.status, `Unable to create ${this.collectionName}`)
@@ -57,12 +61,15 @@ export class BaseDa<T extends IBaseItem, DTO> {
   }
 
   async updateOne(id: ObjectId, payload: DTO): Promise<T> {
+    const cacheKey: string = generateRedisKey(this.cachePrefix)
+
     try {
       const item = await this.database.collection(this.collectionName).findOneAndUpdate(
         {_id: id}, {$set: payload}, {returnDocument: 'after'})
 
-      await redisCache.setJSON(`${this.cacheKey}:${id}`, item.value)
-      // await redisCache.del(`${this.cacheKey}`)
+      await redisCache.reset()
+      await redisCache.setJSON(`${cacheKey}:${id}`, item.value)
+
       return item.value as T
     } catch (error) {
       throw new HttpException(error.status, `Unable to update ${this.collectionName} with id ${id}`)
@@ -70,19 +77,24 @@ export class BaseDa<T extends IBaseItem, DTO> {
   }
 
   async deleteOne(id: ObjectId): Promise<{deleted: boolean}> {
+    const cacheKey: string = generateRedisKey(this.cachePrefix)
+
     try {
       const deleted = await this.database.collection(this.collectionName).deleteOne({_id: id})
 
       if (deleted.deletedCount === 0) {
         return {deleted: false}
       }
-      await redisCache.del(`${this.cacheKey}:${id}`)
 
-      // const fromCache = await redisCache.getJSON(this.cacheKey)
+      await redisCache.reset()
+
+      // await redisCache.del(`${cacheKey}:${id}`)
+
+      // const fromCache = await redisCache.getJSON(cacheKey)
       //
       // if (fromCache) {
       //   const filtered = fromCache.filter(item => item._id !== id.toString())
-      //   await redisCache.setJSON(this.cacheKey, filtered)
+      //   await redisCache.setJSON(cacheKey, filtered)
       // }
 
       return {deleted: true}
